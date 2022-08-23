3bd6d776e15131a3620c5a9ab5fe00a290563a25
SokhibjonDev <110424000+SokhibjonDev@users.noreply.github.com>
2022-08-24 01:04:46 +0500

    feat : the first lesson python || if else

---
+++ 1dars.py
@@ -0,0 +1,35 @@
+"""
+a=int(2) #int---integer butun son
+b=float(1.2) #float o'ngli kast sonlarni o'z ichiga oladigan tur
+c=str('test') #str---string satr yoki matin turi
+d=(a)
+print(type(d))
+
+"""
+"""
+# magazin calculator
+a = str(input('mahsulot nomi: '))
+b = int(input('mahsulot massasi (kg): '))
+kartoshka = int(15000)
+sabzi = int(2000)
+if a == 'kartoshka':
+    c = b*kartoshka
+    print(c, 'so`m')
+elif a == 'sabzi':
+    c = b*sabzi
+    print(c, 'so`m')
+    
+"""
+#mini admin authentication (homework)
+login = 'SokhibjonDev'
+password = 'admin'
+error = 'Login yoki parolda xatolik bor!!!'
+LoginKirish = str(input('Logini kiriting: '))
+PasswordKirish = str(input('Parolni kiriting: '))
+
+if LoginKirish == login:
+    print(PasswordKirish)
+if PasswordKirish == password:
+    print('Success!')
+else:
+    print(error)
