5fb1450aa76b09dc95f5498df4f5da0e2aea7544
SokhibjonDev <110424000+SokhibjonDev@users.noreply.github.com>
2022-08-24 01:06:07 +0500

    feat : dataTypes

---
+++ 1dataTypes.py
@@ -0,0 +1,9 @@
+#Name = False #boolean #logical meaning
+#Name = 22 #intager
+#Name = 2.2 #Float
+#Name = 2+3j #complex
+#Name = 'Alex' #string
+#Name =b"bytes"
+Name = bytes([50,100,76,72,41])
+print(chr(50))
+print(type(Name)) #Check type 
